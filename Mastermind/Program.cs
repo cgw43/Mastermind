9ded75e1c3edb593547e959a14bc3785fb861c52
cgw43 <98435377+cgw43@users.noreply.github.com>
2023-10-04 23:06:46 -0400

    Some Code Cleanup

--- Mastermind/Program.cs
+++ Mastermind/Program.cs
@@ -1,29 +1,32 @@
 ﻿using System;
 using System.Text;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace Mastermind
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             SecretCode sc = new SecretCode();
-            Console.WriteLine(sc.toString());
             Console.WriteLine("Ready to play Mastermind? You have 12 guesses to win");
             if (getUserInput(sc)) {
                 Console.WriteLine("\nYou solved it!\n");
             } else {
                 Console.WriteLine("\nYou lose :(\n");
             }
-            Environment.Exit(0);
+           Console.ReadKey();
         }
 
+        /*
+            Function that gets 12 valid user guesses and tracks progress
+        */
         static bool getUserInput(SecretCode sc) {
 
             int numGuesses = 1;
-            do {
+            while (numGuesses <= 12) {
                 Console.WriteLine("Enter Guess #{0}", numGuesses);
                 string guess = Console.ReadLine();
+                // minor input validations
                 if (guess != null && guess.Length == 4) {
                     numGuesses++;
                     string res = sc.guessResult(guess);
@@ -32,28 +35,34 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 } else {
                     Console.WriteLine("Invalid Guess... try again");
                 }
-            } while (numGuesses <= 12);
+            }
             return false;
         }
 
+        /* Checks guess results to see if user won */
         static bool checkForWin(string guessResult) {
             return guessResult == "++++";
         }
     }
+
+    /* Class to represent the secret code */
     public class SecretCode {
 
         private int[] code;
 
+        /* Constructor randomly generates a secret code */
         public SecretCode() {
             Random rand = new Random();
             int[] temp = {rand.Next(1,7), rand.Next(1,7), rand.Next(1,7), rand.Next(1,7)};
             code = temp;
         }
 
+        /* returns secret code as a string representation (used for testing) */
         public string toString() {
             return "" + code[0] + code[1] + code[2] + code[3];
         }
 
+        /* Checks input guess against secret code and generates result string */
         public string guessResult(string guess) {
             StringBuilder builder = new StringBuilder();
             int[] checkedDigits = new int[6]; 
